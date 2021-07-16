eadc0bdae45b0da47e0cb7725ce8524044f43a0d
Gabriel Henton <gabehenton@gmail.com>
2021-07-15 17:34:49 -0700

    changed the endpoint message

--- api/server.js
+++ api/server.js
@@ -6,7 +6,7 @@ server.use(express.json());
 server.use("/api/user", usersRouter)
 
 server.use("/", (req, res) => {
-    res.status(200).json("Hello!")
+    res.status(200).json("Yes, this works!")
 })
 
 module.exports = server;
